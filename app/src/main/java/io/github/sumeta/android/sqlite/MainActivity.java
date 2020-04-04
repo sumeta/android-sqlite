284ada66692cd24901d9363b10c41c65f2358dc0
sumeta <sumeta.pon@gmail.com>
2020-04-04 22:41:12 +0700

    Add a Floating Action Button

--- app/src/main/java/io/github/sumeta/android/sqlite/MainActivity.java
+++ app/src/main/java/io/github/sumeta/android/sqlite/MainActivity.java
@@ -3,6 +3,10 @@ package io.github.sumeta.android.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.melnykov.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"Add Data",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
